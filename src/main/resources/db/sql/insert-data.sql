bb67fef25095a04bbaaaf30d23fb14ddfd3d5582
Gonzalo E. Ciancio <gonzalociancio@gmail.com>
2017-04-21 18:05:30 -0300

    Modificando insert

--- src/main/resources/db/sql/insert-data.sql
+++ src/main/resources/db/sql/insert-data.sql
@@ -1,5 +1,6 @@
-INSERT INTO person (FIRSTNAME, LASTNAME, EMAIL) VALUES ('Cintia', 'Gioia', 'cgioia@unlam.edu.ar'); 
-INSERT INTO person (FIRSTNAME, LASTNAME, EMAIL) VALUES ('Walter', 'Ureta', 'wureta@unlam.edu.ar');
-INSERT INTO person (FIRSTNAME, LASTNAME, EMAIL) VALUES ('Juan', 'Monteagudo', 'jmonteagudo@unlam.edu.ar');
+INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('Gonzalo', 'Ciancio', 'admin', 'admin', 1, 1, getdate());
+
+INSERT INTO Roles (Descripcion) VALUES (Administrador);
+INSERT INTO Roles (Descripcion) VALUES (Basico);
 
 --COMMIT;
