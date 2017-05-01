79e5e2c7e18a0252c2afe07734ad7c1617b07c52
unlamweb2 <cesardaniel_@msn.com>
2017-05-01 16:44:19 -0300

    Agrego validarUsuario y validarUsuarioAlta
    para validar la carga de datos del login y el registro de usuarios

--- src/main/webapp/js/funciones.js
+++ src/main/webapp/js/funciones.js
@@ -1,18 +1,49 @@
 function validarUsuario()
 {
-	var doc;
-	var pass;
-		
-		doc = document.ingreso.user.value;
-		pass = document.ingreso.contrasena.value;
-		
-		if (doc == '' || pass== '')
+	var usr = document.getElementById("ingreso:mail").value.toString().length; 
+	var pass= document.getElementById("ingreso:password").value.toString().length; 
+			
+		if (usr == 0 || pass== 0)
 		{
 			alert('Debe completar los campos.');
+			/*document.getElementById("ingreso:mail").setAttribute("class","text-danger")
+				document.getElementById("ingreso:password").setAttribute("style", "background-color: red;")*/
 			return false;
 		}
 		else
-		{
-			document.ingreso.submit();
+		{	return true;		
 		}
 }
+
+function validarUsuarioAlta()
+{
+	var nombre = document.getElementById("registro:InNombre").value.toString().length; 
+	var apellido= document.getElementById("registro:InApellido").value.toString().length; 
+	var email = document.getElementById("registro:InEmail").value.toString().length; 
+	var pass= document.getElementById("registro:InPassword").value.toString().length; 
+		
+	if (nombre == 0 )
+	{   alert('Debe ingresar un nombre.');	
+	location.reload();
+		return false;
+	}
+	else if (apellido == 0 )
+	{	alert('Debe ingresar un apellido.');
+	location.reload();
+		return false;		
+	}else if (email == 0 )
+	{   alert('Debe ingresar un Email.');
+	location.reload();
+		return false;
+	}
+	else if (pass < 8 )
+	{	alert('La clave debe contener al menos 8 caracteres');
+	location.reload();
+		return false;	
+	}
+	else
+	{ return true;		
+	}	
+		
+}
+
