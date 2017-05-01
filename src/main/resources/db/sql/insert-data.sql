2ffb911b8d9bafaba1133ba664e1b5b453e5c7c5
radamisil <radamisil@hotmail.com>
2017-05-01 19:46:55 -0300

    se agregan botones funcionales, y se agregan registros

--- src/main/resources/db/sql/insert-data.sql
+++ src/main/resources/db/sql/insert-data.sql
@@ -2,7 +2,7 @@ INSERT INTO Roles (Descripcion) VALUES ('Administrador');
 INSERT INTO Roles (Descripcion) VALUES ('Usuario');
 
 INSERT INTO TiposTarea (Descripcion) VALUES ('Publica');
-INSERT INTO TiposTarea (Descripcion) VALUES ('Solo lectura');
+INSERT INTO TiposTarea (Descripcion) VALUES ('Privada');
 
 INSERT INTO Estados (Descripcion) VALUES ('Pendiente');
 INSERT INTO Estados (Descripcion) VALUES ('Completa');
@@ -13,6 +13,9 @@ INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlt
 INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('Pepe', 'Mujica', 'pepeMujica@gmail.com', 'pepe', 2, false, NULL);
 
 INSERT INTO Tareas (Titulo, Descripcion, EstadoId, TipoTarea, Editable, UsuarioAlta, FechaAlta) VALUES ('Compras', 'Comprar Pan, Leche, Azucar y Cafe.', 1, 1, true, 2, CURDATE());
-INSERT INTO Tareas (Titulo, Descripcion, EstadoId, TipoTarea, Editable, UsuarioAlta, FechaAlta) VALUES ('Estudiar', 'Estudiar para Seguridad.', 1, 2, true, 1, CURDATE());
+INSERT INTO Tareas (Titulo, Descripcion, EstadoId, TipoTarea, Editable, UsuarioAlta, FechaAlta) VALUES ('Estudiar', 'Estudiar para Seguridad.', 1, 1, true, 2, CURDATE());
+INSERT INTO Tareas (Titulo, Descripcion, EstadoId, TipoTarea, Editable, UsuarioAlta, FechaAlta) VALUES ('Trabajar', 'Para conseguir dinero.', 1, 2, true, 1, CURDATE());
+INSERT INTO Tareas (Titulo, Descripcion, EstadoId, TipoTarea, Editable, UsuarioAlta, FechaAlta) VALUES ('Limpiar', 'Para que no haya suciedad.', 1, 2, true, 1, CURDATE());
+INSERT INTO Tareas (Titulo, Descripcion, EstadoId, TipoTarea, Editable, UsuarioAlta, FechaAlta) VALUES ('Transferencia', 'Para pagar auto', 1, 1, true, 1, CURDATE());
 
 --COMMIT;
