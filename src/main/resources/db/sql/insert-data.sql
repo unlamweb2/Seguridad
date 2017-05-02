59c0939e4e7a24cb1ee9d6921514f3c64670f9bb
Gonzalo E. Ciancio <gonzalociancio@gmail.com>
2017-05-02 19:07:32 -0300

    Actualización set de inicialización

--- src/main/resources/db/sql/insert-data.sql
+++ src/main/resources/db/sql/insert-data.sql
@@ -11,14 +11,15 @@ INSERT INTO Estados (Descripcion) VALUES ('Pendiente');
 INSERT INTO Estados (Descripcion) VALUES ('Completa');
 INSERT INTO Estados (Descripcion) VALUES ('Eliminada');
 
-INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('Gonzalo', 'Ciancio', 'admin', 'password', 1, true, '2015-3-12');
-INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('Ramiro', 'Silva', 'ramirosilva@gmail.com', 'hola123', 2, true, '2008-11-11');
-INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('Pepe', 'Mujica', 'pepeMujica@gmail.com', 'pepe', 2, false, NULL);
+INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('Admin', 'Admin', 'admin', 'admin', 1, true, '2010-3-12');
+INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('Usuario', 'Usuario', 'usuario@gmail.com', '123', 2, true, '2011-11-11');
+INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('TestNombre', 'TestApellido', 'testEmail@gmail.com', 'test', 2, false, '2014-11-11');
+INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('Juan', 'Lopez', 'juanlopez@gmail.com', 'password', 2, true, CURDATE());
 
-INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Compras', 'Comprar Pan, Leche, Azucar y Cafe.', 1, 1, 1, 2, CURDATE());
-INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Estudiar', 'Estudiar para Seguridad.', 1, 1, 2, 2, CURDATE());
-INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Trabajar', 'Para conseguir dinero.', 2, 2, 2, 1, CURDATE());
-INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Limpiar', 'Para que no haya suciedad.', 1, 2, 1, 1, CURDATE());
-INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Transferencia', 'Para pagar auto', 2, 1, 1, 1, CURDATE());
+INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Compras', 'Comprar Pan, Leche, Azucar y Cafe.', 1, 1, 1, 2, '2012-11-11');
+INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Estudiar', 'Estudiar para Seguridad.', 1, 1, 2, 2, '2013-01-10');
+INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Trabajar', 'Para conseguir dinero.', 2, 2, 2, 2, CURDATE());
+INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Limpiar', 'Para que no haya suciedad.', 1, 2, 1, 3, CURDATE());
+INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Transferencia', 'Para pagar auto', 2, 1, 1, 3, CURDATE());
 
 --COMMIT;
