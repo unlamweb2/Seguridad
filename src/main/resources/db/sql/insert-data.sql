dee1d53294f1b769ab791419cf45c3c8a7068986
unlamweb2 <cesardaniel_@msn.com>
2017-05-20 13:50:16 -0300

    se agrega diagrama de tablas, script de creacion e inicializacion

--- src/main/resources/db/sql/insert-data.sql
+++ src/main/resources/db/sql/insert-data.sql
@@ -1,25 +1,23 @@
 INSERT INTO Roles (Descripcion) VALUES ('Administrador');
 INSERT INTO Roles (Descripcion) VALUES ('Usuario');
 
-INSERT INTO Privacidad (Descripcion) VALUES ('Publica');
-INSERT INTO Privacidad (Descripcion) VALUES ('Privada');
+INSERT INTO TipoTarea (Descripcion) VALUES ('Publica');
+INSERT INTO TipoTarea (Descripcion) VALUES ('Privada');
 
-INSERT INTO TiposTarea (Descripcion) VALUES ('Editable');
-INSERT INTO TiposTarea (Descripcion) VALUES ('Solo lectura');
+INSERT INTO Estado (Descripcion) VALUES ('Pendiente');
+INSERT INTO Estado (Descripcion) VALUES ('Completa');
+INSERT INTO Estado (Descripcion) VALUES ('Eliminada');
 
-INSERT INTO Estados (Descripcion) VALUES ('Pendiente');
-INSERT INTO Estados (Descripcion) VALUES ('Completa');
-INSERT INTO Estados (Descripcion) VALUES ('Eliminada');
+INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta, Aprobado) VALUES ('Admin', 'Admin', 'admin', 'admin', 1, true, '2010-3-12',true);
+INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta, Aprobado) VALUES ('Usuario', 'Usuario', 'usuario@gmail.com', '123', 2, true, '2011-11-11',false);
+INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta, Aprobado) VALUES ('TestNombre', 'TestApellido', 'testEmail@gmail.com', 'test', 2, false, '2014-11-11',false);
+INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta, Aprobado) VALUES ('Juan', 'Lopez', 'juanlopez@gmail.com', 'password', 2, true, CURDATE(),false);
 
-INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('Admin', 'Admin', 'admin', 'admin', 1, true, '2010-3-12');
-INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('Usuario', 'Usuario', 'usuario@gmail.com', '123', 2, true, '2011-11-11');
-INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('TestNombre', 'TestApellido', 'testEmail@gmail.com', 'test', 2, false, '2014-11-11');
-INSERT INTO Usuarios (Nombre, Apellido, Email, Password, RolId, Activo, FechaAlta) VALUES ('Juan', 'Lopez', 'juanlopez@gmail.com', 'password', 2, true, CURDATE());
 
-INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Compras', 'Comprar Pan, Leche, Azucar y Cafe.', 1, 1, 1, 2, '2012-11-11');
-INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Estudiar', 'Estudiar para Seguridad.', 1, 1, 2, 2, '2013-01-10');
-INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Trabajar', 'Para conseguir dinero.', 2, 2, 2, 2, CURDATE());
-INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Limpiar', 'Para que no haya suciedad.', 1, 2, 1, 3, CURDATE());
-INSERT INTO Tareas (Titulo, Descripcion, EstadoId, Privacidad, TipoTarea, UsuarioAlta, FechaAlta) VALUES ('Transferencia', 'Para pagar auto', 2, 1, 1, 3, CURDATE());
+INSERT INTO Tarea (Titulo, Descripcion, EstadoId, TipoTarea, UsuarioAlta, UsuarioAsignado, FechaAlta) VALUES ('Compras', 'Comprar Pan, Leche, Azucar y Cafe.', 1, 1, 2, 2, '2012-11-11');
+INSERT INTO Tarea (Titulo, Descripcion, EstadoId, TipoTarea, UsuarioAlta, UsuarioAsignado, FechaAlta) VALUES ('Estudiar', 'Estudiar para Seguridad.', 1, 1, 2, 2, '2013-01-10');
+INSERT INTO Tarea (Titulo, Descripcion, EstadoId, TipoTarea, UsuarioAlta, UsuarioAsignado, FechaAlta) VALUES ('Trabajar', 'Para conseguir dinero.', 2, 2, 2, 2, CURDATE());
+INSERT INTO Tarea (Titulo, Descripcion, EstadoId, TipoTarea, UsuarioAlta, UsuarioAsignado, FechaAlta) VALUES ('Limpiar', 'Para que no haya suciedad.', 1, 2, 3, 3, CURDATE());
+INSERT INTO Tarea (Titulo, Descripcion, EstadoId, TipoTarea, UsuarioAlta, UsuarioAsignado, FechaAlta) VALUES ('Transferencia', 'Para pagar auto', 2, 1, 3, 3, CURDATE());
 
 --COMMIT;
